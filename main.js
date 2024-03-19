fe646d5a933764996f0b8c42ed4f0e623a08af0f
tar.gergo <tar.gergo21@gmail.com>
2024-03-19 15:35:57 +0100

    játékosok kész(két játékos játszik nyerés)

--- main.js
+++ main.js
@@ -26,32 +26,188 @@ jatekTerTELEM.addEventListener("click", function (event) {
     db++;
     console.log(PALYA);
     console.log(aktIndex);
-  } else if (db % 2 === 0 && PALYA[aktIndex] === " ") {
-    let valtas = "O";
-    event.target.innerHTML = "O";
-    PALYA[aktIndex] = valtas;
-    db++;
-    console.log(PALYA);
-    console.log(aktIndex);
+    nyerte(PALYA);
+  }else if(db % 2 === 0 && PALYA[aktIndex] === " "){
+  let valtas = "O";
+  event.target.innerHTML = "O";
+  PALYA[aktIndex] = valtas;
+  nyerte(PALYA);
+  db++;
   }
 });
+gepLep(db);
+
 function nyerte(PALYA) {
   let jatekos = "X";
   let gep = "O";
   //visszintbe
-  if (PALYA[(0, 1, 2)] === jatekos || PALYA[(0, 1, 2)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
-  } else if (PALYA[(3, 4, 5)] === jatekos || PALYA[(3, 4, 5)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
-  } else if (PALYA[(6, 7, 8)] === jatekos || PALYA[(6, 7, 8)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
-  } 
+  if (PALYA[0] === jatekos && PALYA[1] === jatekos && PALYA[2] === jatekos) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[0] === gep && PALYA[1] === gep && PALYA[2] === gep) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA[3] === jatekos &&
+    PALYA[4] === jatekos &&
+    PALYA[5] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[3] === gep && PALYA[4] === gep && PALYA[5] === gep) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA[6] === jatekos &&
+    PALYA[7] === jatekos &&
+    PALYA[8] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[6] === gep && PALYA[7] === gep && PALYA[8] === gep) {
+    return alert(`Gép Nyert!`);
+  }
   //függölegesbe
-  else if (PALYA[(0, 3, 6)] === jatekos || PALYA[(0, 3, 6)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
-  } else if (PALYA[(2, 5, 8)] === jatekos || PALYA[(2, 5, 8)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
-  } else if (PALYA[(1, 4, 7)] === jatekos || PALYA[(1, 4, 7)] === gep) {
-    alert(`${jatekos} Nyert!`) || alert(`${gep} Nyert!`);
+  else if (
+    PALYA[0] === jatekos &&
+    PALYA[3] === jatekos &&
+    PALYA[6] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[0] === gep && PALYA[3] === gep && PALYA[6] === gep) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA[2] === jatekos &&
+    PALYA[5] === jatekos &&
+    PALYA[8] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[2] === gep && PALYA[5] === gep && PALYA[8] === gep) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA[1] === jatekos &&
+    PALYA[4] === jatekos &&
+    PALYA[7] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[1] === gep && PALYA[4] === gep && PALYA[7] === gep) {
+    return alert(`Gép Nyert!`);
+  }
+  //keresztbe
+  else if (
+    PALYA[0] === jatekos &&
+    PALYA[4] === jatekos &&
+    PALYA[8] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[0] === gep && PALYA[4] === gep && PALYA[8] === gep) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA[6] === jatekos &&
+    PALYA[4] === jatekos &&
+    PALYA[2] === jatekos
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (PALYA[6] === gep && PALYA[4] === gep && PALYA[2] === gep) {
+    return alert(`Gép Nyert!`);
+  }
+  //döntetlenek
+  //visszint
+  else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[0] === jatekos &&
+        PALYA[1] === jatekos &&
+        PALYA[2] === jatekos
+      )) ||
+    (PALYA[0] === gep && PALYA[1] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  } else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[3] === jatekos &&
+        PALYA[4] === jatekos &&
+        PALYA[5] === jatekos
+      )) ||
+    (PALYA[3] === gep && PALYA[4] === gep && PALYA[5] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  } else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[6] === jatekos &&
+        PALYA[7] === jatekos &&
+        PALYA[8] === jatekos
+      )) ||
+    (PALYA[6] === gep && PALYA[7] === gep && PALYA[8] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  }
+  //függöleges
+  else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[0] === jatekos &&
+        PALYA[3] === jatekos &&
+        PALYA[6] === jatekos
+      )) ||
+    (PALYA[0] === gep && PALYA[3] === gep && PALYA[6] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  } else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[2] === jatekos &&
+        PALYA[5] === jatekos &&
+        PALYA[8] === jatekos
+      )) ||
+    (PALYA[2] === gep && PALYA[5] === gep && PALYA[8] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  } else if (
+    (uresPalya(PALYA) === false &&
+      !(
+        PALYA[1] === jatekos &&
+        PALYA[4] === jatekos &&
+        PALYA[7] === jatekos
+      )) ||
+    (PALYA[1] === gep && PALYA[4] === gep && PALYA[7] === gep)
+  ) {
+    return alert(`Döntetlen`);
+  } else if (
+    PALYA === false &&
+    !(PALYA[0] === jatekos && PALYA[4] === jatekos && PALYA[8] === jatekos)
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (
+    PALYA === false &&
+    !(PALYA[0] === gep && PALYA[4] === gep && PALYA[8] === gep)
+  ) {
+    return alert(`Gép Nyert!`);
+  } else if (
+    PALYA === false &&
+    !(PALYA[6] === jatekos && PALYA[4] === jatekos && PALYA[2] === jatekos)
+  ) {
+    return alert(`Játékos Nyert!`);
+  } else if (
+    PALYA === false &&
+    !(PALYA[6] === gep && PALYA[4] === gep && PALYA[2] === gep)
+  ) {
+    return alert(`Gép Nyert!`);
   }
 }
+
+function uresPalya(PALYA) {
+  return PALYA.includes(" ");
+}
+/*function gepLep(db) {
+  let randomHely = Math.floor(Math.random()*9);
+  while (PALYA[randomHely] === " ") {
+    randomHely = Math.floor(Math.random(0, 9));
+  }
+  if (db % 2 === 0 && PALYA[randomHely] === " ") {
+    
+  }
+ 
+  let valtas = "O";
+  PALYA[randomHely] = valtas;
+  nyerte(PALYA);
+  db++;
+  return PALYA[randomHely] = valtas
+}*/
